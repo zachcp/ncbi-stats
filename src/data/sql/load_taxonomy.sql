66ce9e661bd56e8852d87de6952cc8f51b97cbc5
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-01-21 11:52:30 -0500

    fix hte taxonomy

--- src/data/sql/load_taxonomy.sql
+++ src/data/sql/load_taxonomy.sql
@@ -1,21 +1,29 @@
+CREATE
+OR REPLACE TABLE raw_taxonomy (line VARCHAR);
+
+-- Load the entire file as single lines
+COPY raw_taxonomy
+FROM
+    'rankedlineage.dmp' (
+        DELIMITER '', -- Read entire lines
+        AUTO_DETECT false
+    );
+
 DROP TABLE IF EXISTS ncbi_taxonomy;
 
 CREATE TABLE ncbi_taxonomy AS
 SELECT
-    CAST(SPLIT_PART (line, E '\t|\t', 1) AS INTEGER) as tax_id,
-    SPLIT_PART (line, E '\t|\t', 2) as tax_name,
-    SPLIT_PART (line, E '\t|\t', 3) as species,
-    SPLIT_PART (line, E '\t|\t', 4) as genus,
-    SPLIT_PART (line, E '\t|\t', 5) as family,
-    SPLIT_PART (line, E '\t|\t', 6) as "order",
-    SPLIT_PART (line, E '\t|\t', 7) as class,
-    SPLIT_PART (line, E '\t|\t', 8) as phylum,
-    SPLIT_PART (line, E '\t|\t', 9) as kingdom,
-    REGEXP_REPLACE (SPLIT_PART (line, E '\t|\t', 10), '\t\|$', '') as superkingdom
+    CAST(SPLIT_PART (line, E'\t|\t', 1) AS INTEGER) as tax_id,
+    SPLIT_PART (line, E'\t|\t', 2) as tax_name,
+    SPLIT_PART (line, E'\t|\t', 3) as species,
+    SPLIT_PART (line, E'\t|\t', 4) as genus,
+    SPLIT_PART (line, E'\t|\t', 5) as family,
+    SPLIT_PART (line, E'\t|\t', 6) as "order",
+    SPLIT_PART (line, E'\t|\t', 7) as class,
+    SPLIT_PART (line, E'\t|\t', 8) as phylum,
+    SPLIT_PART (line, E'\t|\t', 9) as kingdom,
+    REGEXP_REPLACE (SPLIT_PART (line, E'\t|\t', 10), '\t\|$', '') as superkingdom
 FROM
-    read_csv_auto (
-        'rankedlineage.dmp',
-        sep = '\t',
-        header = false,
-        skip = 2
-    );
+    raw_taxonomy;
+
+DROP TABLE IF EXISTS raw_taxonomy;
