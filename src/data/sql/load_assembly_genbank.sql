cb5de441f99623abd15128a741d605301c6c3ed5
Zachary Charlop-Powers <zach.charlop.powers@gmail.com>
2025-01-21 13:17:36 -0500

    updat the sql

--- src/data/sql/load_assembly_genbank.sql
+++ src/data/sql/load_assembly_genbank.sql
@@ -8,8 +8,8 @@ SELECT
     "column02" as biosample,
     "column03" as wgs_master,
     "column04" as refseq_category,
-    "column05" as tax_id,
-    "column06" as species_taxid,
+    CAST("column05" AS INTEGER) as tax_id, -- int
+    CAST("column06" AS INTEGER) as species_taxid, -- int
     "column07" as organism_name,
     "column08" as infraspecific_name,
     "column09" as isolate,
@@ -17,14 +17,40 @@ SELECT
     "column11" as assembly_level,
     "column12" as release_type,
     "column13" as genome_rep,
-    "column14" as seq_rel_date,
+    CAST("column14" AS DATE) as seq_rel_date,
+    EXTRACT(
+        YEAR
+        FROM
+            CAST("column14" AS DATE)
+    ) as year,
+    EXTRACT(
+        MONTH
+        FROM
+            CAST("column14" AS DATE)
+    ) as month,
     "column15" as asm_name,
     "column16" as submitter,
     "column17" as gbrs_paired_asm,
     "column18" as paired_asm_comp,
     "column19" as ftp_path,
     "column20" as excluded_from_refseq,
-    "column21" as relation_to_type_material
+    "column21" as relation_to_type_material, -- enum
+    "column22" as asm_not_live_date,
+    "column23" as assembly_type, -- enum
+    "column24" as group, -- very useful for sorting.
+    "column25" AS INTEGER as genome_size, -- int
+    "column26" as genome_size_ungapped, -- int
+    "column27" as gc_percent, --f32
+    "column28" as replicon_count, -- smallint
+    "column29" as scaffold_count, -- small int
+    "column30" as contig_count, -- small int
+    "column31" as annotation_provider, -- str
+    "column32" as annotation_name,
+    "column33" as annotation_date,
+    TRY_CAST ("column34" AS BIGINT) as total_gene_count, -- int
+    "column35" as protein_coding_gene_count, --int
+    "column36" as non_coding_gene_count, --int
+    "column37" as pubmed_id -- comma separated IDs
 FROM
     read_csv_auto (
         'assembly_summary_genbank.txt',
