aafef858fd8aa72712dc1f61fb2ebfe3225e1eb2
zachcp <zachcp@users.noreply.github.com>
2025-01-26 15:26:40 -0500

    Add Enums to a few columns (#8)
    * shave down 50-70MB
    
    * beign factoring out the ENUMs
    
    * a few more enums and another 10-30MB
    
    * update documentation of the columns

---
+++ src/data/sql/enums.sql
@@ -0,0 +1,34 @@
+CREATE TYPE assembly_level_enum AS ENUM (
+    'Complete Genome',
+    'Chromosome',
+    'Scaffold',
+    'Contig'
+);
+
+CREATE TYPE assembly_type_enum AS ENUM (
+    'haploid',
+    'haploid-with-alt',
+    'haploid-with-alt-loci',
+    'diploid',
+    'unresolved-diploid',
+    'alternate-pseudohaplotype'
+);
+
+-- CREATE TYPE excluded_from_refseq_enum AS ENUM ('Full', 'Partial');
+CREATE TYPE genome_rep_enum AS ENUM ('Full', 'Partial');
+
+CREATE TYPE relation_to_type_material_enum AS ENUM (
+    'assembly from type material',
+    'assembly from synonym type material',
+    'assembly from pathotype material',
+    'assembly designated as clade exemplar',
+    'assembly designated as neotype',
+    'assembly designated as reftype',
+    'ICTV species exemplar',
+    'ICTV additional isolate',
+    'na'
+);
+
+CREATE TYPE release_type_enum AS ENUM ('Major', 'Minor', 'Patch');
+
+CREATE TYPE version_status_enum AS ENUM ('latest', 'replaced', 'suppressed');
--- src/data/sql/load_assembly_refseq.sql
+++ src/data/sql/load_assembly_refseq.sql
@@ -3,9 +3,30 @@ DROP TABLE IF EXISTS assembly_summary_refseq;
 
 CREATE TABLE assembly_summary_refseq AS
 SELECT
-    "column00" as assembly_accession,
-    "column01" as bioproject,
-    "column02" as biosample,
+    -- "column00" as assembly_accession,
+    REGEXP_EXTRACT ("column00", '^(GCA|GCF)', 0) as assembly_accession_loc,
+    CAST(
+        REGEXP_EXTRACT ("column00", '_(\d+)\.', 1) AS INTEGER
+    ) as assembly_accession_int,
+    CAST(
+        REGEXP_EXTRACT ("column00", '\.(\d+)$', 1) AS INTEGER
+    ) as assembly_accession_version,
+
+    -- "column01" as bioproject,
+    -- Convert this into 1 column: PRJNA13184 => 13184
+    CASE
+        WHEN "column01" = '' THEN NULL
+        ELSE CAST(
+            NULLIF(REGEXP_EXTRACT ("column01", 'PRJNA(\d+)', 1), '') AS INTEGER
+        )
+    END as bioproject,
+    -- "column02" as biosample,
+    CASE
+        WHEN "column01" = '' THEN NULL
+        ELSE CAST(
+            NULLIF(REGEXP_EXTRACT ("column02", 'SAMN(\d+)', 1), '') AS INTEGER
+        )
+    END as biosample,
     "column03" as wgs_master,
     "column04" as refseq_category,
     CAST("column05" AS INTEGER) as tax_id, -- int
@@ -13,10 +34,10 @@ SELECT
     "column07" as organism_name,
     "column08" as infraspecific_name,
     "column09" as isolate,
-    "column10" as version_status,
-    "column11" as assembly_level,
-    "column12" as release_type,
-    "column13" as genome_rep,
+    CAST("column10" as version_status_enum) as version_status,
+    CAST("column11" as assembly_level_enum) as assembly_level,
+    CAST("column12" as release_type_enum) as release_type,
+    CAST("column13" as genome_rep_enum)  as genome_rep,
     CAST("column14" AS DATE) as seq_rel_date,
     EXTRACT(
         YEAR
@@ -32,11 +53,11 @@ SELECT
     "column16" as submitter,
     "column17" as gbrs_paired_asm,
     "column18" as paired_asm_comp,
-    "column19" as ftp_path,
+    REPLACE("column19", 'https://ftp.ncbi.nlm.nih.gov/genomes/all/', '') as ftp_path,
     "column20" as excluded_from_refseq,
-    "column21" as relation_to_type_material, -- enum
+    CAST("column21" AS relation_to_type_material_enum) as relation_to_type_material, -- enum
     "column22" as asm_not_live_date,
-    "column23" as assembly_type, -- enum
+    CAST("column23" AS assembly_type_enum) as assembly_type, -- enum
     "column24" as group, -- very useful for sorting.
     CAST("column25" AS BIGINT) as genome_size, -- int
     "column26" as genome_size_ungapped, -- int
